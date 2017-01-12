bb0c562a6e1ff09d8b479af6d57aa435afa289e5
saidevpo <saidev.polavarapu@gmail.com>
2017-01-12 11:30:51 -0500

    Added new print statement and couple of characters

--- UblifeProject/src/com/saidev/practice/SharedTest.java
+++ UblifeProject/src/com/saidev/practice/SharedTest.java
@@ -3,9 +3,10 @@ package com.saidev.practice;
 public class SharedTest {
 
 	public static void main(String[] args) {
-		String [] names = {"sai","avaneesh","suma"};
+		String [] names = {"sai","avaneesh","suma","kp","girija"};
 		for (int i = 0; i < names.length; i++) {
 			System.out.println(names[i]);
+			System.out.println("Second print statement");
 		}
 	}
 
