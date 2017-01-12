e94a45bc2fd8c3baf3416bb21d57f0417a61ba68
saidevpo <saidev.polavarapu@gmail.com>
2017-01-12 11:17:54 -0500

    Developer1 commit

---
+++ UblifeProject/src/com/saidev/practice/SharedTest.java
@@ -0,0 +1,12 @@
+package com.saidev.practice;
+
+public class SharedTest {
+
+	public static void main(String[] args) {
+		String [] names = {"sai","avaneesh","suma"};
+		for (int i = 0; i < names.length; i++) {
+			System.out.println(names[i]);
+		}
+	}
+
+}
